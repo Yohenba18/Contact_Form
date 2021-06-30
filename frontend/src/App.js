fa43f49bf9c8c05ecdbebc7427f4ddc60f8096c0
Yohenba18 <roddik12kshetri@gmail.com>
2021-06-30 15:11:30 +0530

    functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
 
-  const [state,setState] = useState({
-    name: '',
-    email: '',
-    subject: '',
-    message: '',
-  })
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log(state.name);
+  };
 
-  
+  const stateChange = (e) => {
+    const { name, value } = e.target;
+
+    setState({
+      ...state,
+      [name]: value
+    });
+  };
 
   return (
     <div className="app">
       <div className="contactMe">
         <form>
-          <h1>CONTACT ME</h1>
-          <input type="text" placeholder="Full Name" />
-          <input type="text" placeholder="Email ID" />
-          <input type="text" placeholder="Subject" />
-          <textarea type="text" placeholder="Type your message here..." />
-          <button>Submit</button>
+          <h1>Let's connect</h1>
+          <input
+            type="text"
+            placeholder="Full Name"
+            value={state.name}
+            onChange={stateChange}
+          />
+          <input
+            type="text"
+            value={state.email}
+            onChange={stateChange}
+            placeholder="Email ID"
+          />
+          <input
+            type="text"
+            value={state.subject}
+            onChange={stateChange}
+            placeholder="Subject"
+          />
+          <textarea
+            type="text"
+            value={state.message}
+            onChange={stateChange}
+            placeholder="Type your message here..."
+          />
+          <button onClick={sendEmail} type="submit">
+            Submit
+          </button>
         </form>
       </div>
     </div>
