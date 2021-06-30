40e2e1c4357ba68395fbffb8a808810c6275ab4a
Yohenba18 <roddik12kshetri@gmail.com>
2021-06-30 23:45:47 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -1,26 +1,17 @@
 const express = require("express");
-const router = express.Router();
-const cors = require("cors");
 const nodemailer = require("nodemailer");
+const path = require("path");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
 
-const contactEmail = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "roddik12kshetri@gmail.com",
-    pass: "Yohen4Korou",
-  },
+const buildPath = path.join(__dirname, "..", "build");
+app.use(express.json());
+
+app.post("/send", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
 
-contactEmail.verify((error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Ready to Send");
-  }
+app.listen(3030, () => {
+  console.log("server start on port 3030");
 });
