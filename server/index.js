5569be13d1e4e0d2eea6f9095bfda59f66e8d352
Yohenba18 <roddik12kshetri@gmail.com>
2021-07-01 20:05:19 +0530

    updating post in server.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const transporter = require('./config.js');
+const transporter = require("./config.js");
 
 const buildPath = path.join(__dirname, "..", "build");
 
@@ -11,8 +11,29 @@ app.use(express.static(buildPath));
 app.get("/", (req, res) => res.status(200).send("Hello World"));
 
 app.post("/send", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+  try {
+    const mailOptions = {
+      from: req.body.email,
+      to: process.env.email,
+      subject: req.body.subject,
+      html: `<p>You have a new connect request</p>
+      <h3>Contact details</h3>
+      <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Subject: ${req.body.subject}</li>
+        <li>Message: ${req.body.message}</li>
+      </ul>`,
+    };
+
+    transporter.sendMail()
+  } catch (error) {
+    res.status(500);
+    res.send({
+      sucess: false,
+      message: 'Something went wrong.Please try again'
+    });
+  }
 });
 
 app.listen(3030, () => {
